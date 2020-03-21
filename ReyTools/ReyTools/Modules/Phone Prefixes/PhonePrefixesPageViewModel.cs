af589efae0368958ee5847ac61807c9e44a95a8f
Reynold <reydev.sourcecoder@gmail.com>
2020-03-21 16:55:06 +0800

    Mobile Prefixes version 1.0.0.0

--- ReyTools/ReyTools/Modules/Phone Prefixes/PhonePrefixesPageViewModel.cs
+++ ReyTools/ReyTools/Modules/Phone Prefixes/PhonePrefixesPageViewModel.cs
@@ -1,12 +1,39 @@
 ﻿using Prism.Navigation;
 using ReyTools.Data;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace ReyTools.ViewModels
 {
     public class PhonePrefixesPageViewModel : ViewModelBase
     {
-        public ObservableCollection<MobilePrefix> MobilePrefixes { get; set; }
+
+        private ObservableCollection<MobilePrefix> _mobilePrefixes;
+        public ObservableCollection<MobilePrefix> MobilePrefixes
+        {
+            get { return _mobilePrefixes; }
+            set { _mobilePrefixes = value; RaisePropertyChanged(); }
+        }
+
+        private ObservableCollection<MobilePrefix> _filteredMobilePrefixes;
+        public ObservableCollection<MobilePrefix> FilteredMobilePrefixes
+        {
+            get { return _filteredMobilePrefixes; }
+            set { _filteredMobilePrefixes = value; RaisePropertyChanged(); }
+        }
+
+        private string _prefixKey;
+        public string PrefixKey
+        {
+            get { return _prefixKey; }
+            set 
+            { 
+                _prefixKey = value;
+                RaisePropertyChanged();
+                FilterMobilePrefixes(value);
+            }
+        }
+
 
         public PhonePrefixesPageViewModel(INavigationService navigationService) : base(navigationService)
         {
@@ -16,6 +43,15 @@ namespace ReyTools.ViewModels
         public override void InitializePageBinding(INavigationParameters parameters)
         {
             MobilePrefixes = new ObservableCollection<MobilePrefix>(FakeData.MobilePrefixes);
+            FilteredMobilePrefixes = new ObservableCollection<MobilePrefix>(MobilePrefixes);
+        }
+
+        private void FilterMobilePrefixes(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                FilteredMobilePrefixes = MobilePrefixes;
+            else
+                FilteredMobilePrefixes = new ObservableCollection<MobilePrefix>(MobilePrefixes.Where(p => p.Prefix.Contains(key)));
         }
     }
 }
